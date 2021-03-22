e4f70688e6b02cec485cad39806861d37280f268
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-22 16:46:11 +0100

    python3Packages.semver: 2.10.2 -> 2.13.0

--- pkgs/development/python-modules/semver/default.nix
+++ pkgs/development/python-modules/semver/default.nix
@@ -1,23 +1,29 @@
 { lib
-, fetchFromGitHub
 , buildPythonPackage
+, fetchFromGitHub
+, pytest-cov
 , pytestCheckHook
-, pytestcov
 }:
 
 buildPythonPackage rec {
   pname = "semver";
-  version = "2.10.2";
+  version = "2.13.0";
 
   src = fetchFromGitHub {
     owner = "python-semver";
     repo = "python-semver";
     rev = version;
-    sha256 = "0yxjmcgk5iwp53l9z1cg0ajrj18i09ircs11ifpdrggzm8n1blf3";
+    sha256 = "sha256-IWTo/P9JRxBQlhtcH3JMJZZrwAA8EALF4dtHajWUc4w=";
   };
 
+  checkInputs = [
+    pytest-cov
+    pytestCheckHook
+  ];
+
   preCheck = "rm -rf dist"; # confuses source vs dist imports in pytest
-  checkInputs = [ pytestCheckHook pytestcov ];
+
+  pythonImportsCheck = [ "semver" ];
 
   meta = with lib; {
     description = "Python package to work with Semantic Versioning (http://semver.org/)";
