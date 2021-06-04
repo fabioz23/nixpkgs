88acd8162a7959752c5fc3459345543a5f8486d6
Fabian Affolter <mail@fabian-affolter.ch>
2021-06-04 13:49:02 +0200

    python3Packages.aiofiles: 0.6.0 -> 0.7.0

--- pkgs/development/python-modules/aiofiles/default.nix
+++ pkgs/development/python-modules/aiofiles/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "aiofiles";
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e0281b157d3d5d59d803e3f4557dcc9a3dff28a4dd4829a9ff478adae50ca092";
+    sha256 = "sha256-ocT8my/4FWjIPiE5KoLzROqdI9qQbk9qUmYnZFReGdQ=";
   };
 
   disabled = pythonOlder "3.3";
