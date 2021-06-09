4261722f1526419cb4433f0ecdbbe0a134573f0f
Fabian Affolter <mail@fabian-affolter.ch>
2021-06-09 09:34:55 +0200

    python3Packages.typing-inspect: 0.6.0 -> 0.7.1

--- pkgs/development/python-modules/typing-inspect/default.nix
+++ pkgs/development/python-modules/typing-inspect/default.nix
@@ -3,17 +3,17 @@
 , fetchPypi
 , typing-extensions
 , mypy-extensions
-, isPy39
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "typing-inspect";
-  version = "0.6.0";
+  version = "0.7.1";
 
   src = fetchPypi {
     inherit version;
     pname = "typing_inspect";
-    sha256 = "1dzs9a1pr23dhbvmnvms2jv7l7jk26023g5ysf0zvnq8b791s6wg";
+    sha256 = "sha256-BH1Al9mxf0ZTG/bwFDVhEaG2+4IaJP56yQmFPKKngqo=";
   };
 
   propagatedBuildInputs = [
@@ -21,11 +21,16 @@ buildPythonPackage rec {
     mypy-extensions
   ];
 
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "typing_inspect" ];
+
   meta = with lib; {
     description = "Runtime inspection utilities for Python typing module";
     homepage = "https://github.com/ilevkivskyi/typing_inspect";
     license = licenses.mit;
     maintainers = with maintainers; [ albakham ];
-    broken = isPy39;  # see https://github.com/ilevkivskyi/typing_inspect/issues/65
   };
 }
